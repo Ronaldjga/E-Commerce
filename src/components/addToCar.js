e994cb1fa1e8d567cec648a542687d4ebb19fcca
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-04-14 10:17:36 -0300

    atualização addToCart

--- src/components/addToCar.js
+++ src/components/addToCar.js
@@ -9,9 +9,14 @@ export function AddToCar({ thisGame, thisAmount}) {
         if (!localStorage.getItem('myShoppingCart')) {
             setButtonValue('Adicionar ao carrinho')
         } else {
-            setButtonValue('Atualizar carrinho')
+            const allProducts = JSON.parse(localStorage.getItem('myShoppingCart'))
+            if (allProducts.find(e => e.game.name === thisGame.name && e.quantidade !== thisAmount)) {
+                setButtonValue('Atualizar carrinho')
+            } else if (allProducts.find(e => e.game.name === thisGame.name && e.quantidade === thisAmount)) {
+                setButtonValue('Quantidade já adicionada')
+            }
         }
-    }, [])
+    }, [thisAmount])
 
     return (
         <button
@@ -21,35 +26,23 @@ export function AddToCar({ thisGame, thisAmount}) {
                 if (!localStorage.getItem('myShoppingCart')) {
                     const toArray = [{"game": thisGame, "quantidade": thisAmount}]
                     localStorage.setItem('myShoppingCart', JSON.stringify(toArray))
-                    console.log('não existe')
+                    setButtonValue('Quantidade já adicionada')
                     console.log(toArray)
                 } else {
                     setButtonValue(`Atualizar o carinho`)
                     if (allProducts.find(e => e.game.name === thisGame.name && e.quantidade === thisAmount)) {
+                        setButtonValue('Quantidade ja adicionada')
                         return
                     } else if (allProducts.find(e => e.game.name === thisGame.name && e.quantidade !== thisAmount)) {
                         allProducts.map((data) => data.game.name === thisGame.name ? data.quantidade= thisAmount : null)
                         const toArray = [...allProducts]
                         localStorage.setItem('myShoppingCart', JSON.stringify(toArray))
+                        setButtonValue('Quantidade ja adicionada')
                     } else if (allProducts.find(e => e.game.name !== thisGame.name)) {
                         const toArray = [...allProducts, {"game": thisGame, "quantidade": thisAmount}]
                         localStorage.setItem('myShoppingCart', JSON.stringify(toArray))
                     }
-                    // allProducts.map((data, key) => {
-                    //     if (data.game.name === thisGame.name && data.quantidade !== thisAmount) {
-                    //         data.quantidade = thisAmount
-                    //         const toArray = [...allProducts]
-                    //         localStorage.setItem('myShoppingCart', JSON.stringify(toArray))
-                    //         console.log('ta execultando')
-                        
-                    //     } else if (data.game.name.includes(thisGame.name)) {
-                    //         return
-                    //     } else {
-                    //         const toArray = [...allProducts, {"game": thisGame, "quantidade": thisAmount}]
-                    //         localStorage.setItem('myShoppingCart', JSON.stringify(toArray))
-                    //     }
-                    //     console.log(data.game.name.includes(thisGame.name))
-                    // })
+                    
                 }
                 
             }}
